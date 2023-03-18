0aa7ea6f495f09803d0f2933aaf050668a7b093c
CinisArboris <eyver.evm@gmail.com>
2023-03-18 12:08:58 -0400

    update : refactor code

--- parasite.py
+++ parasite.py
@@ -1,76 +1,64 @@
-#import
 from PIL import Image
-import numpy
 
-class Parasite :	
-	#variables
-	fila		= -1#default
-	columna		= -1#default
-	imagenPNG	= -1#default
-	imagenPX	= -1#default
-	cadenaSTR	= -1#default
-	vectorDEC	= []
-	
-	
-	def __init__ (self, fila, columna, cadenaSTR, vectorDEC):
-		self.fila		= fila
-		self.columna	= columna
-		self.cadenaSTR	= cadenaSTR
-		self.vectorDEC	= vectorDEC
-	# ------------------------------------------------
-	def guardarCerrar(self):
-		self.imagenPNG.save('nox.png')
-	# ------------------------------------------------
-	def banner(self):
+
+class Parasite:
+	gFil = -1
+	gCol = -1
+	gInp = -1
+	gNam = ''
+
+	def __init__(self, fil, col, inp, nam):
+		if fil*col < len(inp):
+			print('Error: la imagen no es suficientemente grande.')
+			print('Longitud del mensaje: ', len(inp))
+			print('Dimensiones de la imagen: ', fil, 'x', col, '=', fil*col)
+			exit(1)
+		self.gFil = fil
+		self.gCol = col
+		self.gInp = inp
+		self.gNam = nam
+
+	@staticmethod
+	def banner():
 		for a in range(10):
-			print (' '*a+"You did't say the magic word !!!")
-	# ------------------------------------------------
-	def crearImagen(self):
-		dimensiones=(self.fila, self.columna)
-		self.imagenPNG = Image.new(
-							'RGB',
-							dimensiones,
-							'white'
-						)
-		self.imagenPX = self.imagenPNG.load()
-		self.guardarCerrar()
+			print(' '*a+"You didn't say the magic word !!!")
 
-	def strTObin (self):
-		for x in self.cadenaSTR:
-			self.vectorDEC.append(bin(ord(x)))#[2:])
-	# ------------------------------------------------
-	def insertarDatos(self):
-		sum = 0
-		self.imagenPNG = Image.open('nox.png')
-		self.imagenPX = self.imagenPNG.load()
-		tope = len(self.vectorDEC)
-		inicio = 0
-		while (inicio < tope):
-			fila = 0
-			columna = 0
-			while (fila < self.fila):
-				columna = 0
-				while (columna < self.columna):
-					if inicio == tope:
-						self.guardarCerrar()
-						return 'fin'
-					x = self.vectorDEC[inicio]
-					inicio = inicio + 1
-					x = int(x,2)
-					r,g,b = (self.imagenPX[fila,columna])
-					self.imagenPX[fila,columna] = (x,x,x)
-					columna = columna + 1
-					sum = sum + 1
-				fila = fila + 1
-			inicio = inicio + 1
-		self.guardarCerrar()
-# ================================================
-# =============     M A I N     ==================
-parasite = Parasite(10, 10, "Buenas noches mi amor :)", [])
+	def crearImagen(self):
+		dimensiones = (self.gFil, self.gCol)
+		imagenObj = Image.new('RGB', dimensiones, 'red')
+		self.guardarCerrar(imagenObj)
+
+	def guardarCerrar(self, imagenObj):
+		imagenObj.save(self.gNam)
+
+	def strTObin(self) -> list:
+		vectorDecimal = []
+		for x in self.gInp:
+			vectorDecimal.append(bin(ord(x)))
+		print('vectorDecimal: ', vectorDecimal)
+		return vectorDecimal
+
+	def insertarDatos(self, vectorDecimal):
+		imagenObj = Image.open(self.gNam)
+		imagenPx = imagenObj.load()
+		iteraciones = len(vectorDecimal)
+		count = 0
+		if count < iteraciones:
+			for fila in range(self.gFil):
+				for columna in range(self.gCol):
+					if count == iteraciones:
+						self.guardarCerrar(imagenObj)
+						print('Imagen guardada con exito.')
+						exit(0)
+					imagenPx[fila, columna] = (int(vectorDecimal[count], 2), int(vectorDecimal[count], 2), int(vectorDecimal[count], 2))
+					count = count + 1
+		self.guardarCerrar(imagenObj)
+
+
+parasite = Parasite(10, 10, '[fortis fortuna adiuvat]', 'nox.png')
 parasite.banner()
 parasite.crearImagen()
-parasite.strTObin()
-parasite.insertarDatos()
+parasite.insertarDatos(parasite.strTObin())
 
 
 
